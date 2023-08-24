76205d5b50f42de11858acba485d9142d1046071
Tricui11 <karitrust1@gmail.com>
2023-08-25 01:14:43 +0300

    UI improved

--- dialog.cpp
+++ dialog.cpp
@@ -1,11 +1,12 @@
 #include <dialog.h>
 #include <ui_dialog.h>
 #include <QMessageBox>
-#include <cmath>
+#include <QButtonGroup>
 #include <QPainter>
 #include <QRegularExpression>
-#include <edmondskarpnetflow.cpp>
+#include <cmath>
 #include <iostream>
+#include <edmondskarpnetflow.cpp>
 #include <dinicnetflow.cpp>
 
 Dialog::Dialog(QWidget *parent)
@@ -20,6 +21,11 @@ Dialog::Dialog(QWidget *parent)
     ui->lineEdit_vertex1->setValidator(cellValidator);
     ui->lineEdit_vertex_2->setValidator(cellValidator);
     ui->lineEdit_weight->setValidator(cellValidator);
+
+    QButtonGroup *buttonGroup = new QButtonGroup(this);
+    buttonGroup->addButton(ui->radioButton_EdmondsKarp);
+    buttonGroup->addButton(ui->radioButton_Dinic);
+    ui->radioButton_EdmondsKarp->setChecked(true);
 }
 
 Dialog::~Dialog()
@@ -28,41 +34,40 @@ Dialog::~Dialog()
 }
 
 int matrix[MAXV][MAXV];
-FlowGraph graph(MAXV, false);
-EdmondsKarpNetflow *netflow = new EdmondsKarpNetflow();
-
-FlowGraph getGraph()
-{
-    return graph;
-}
+FlowGraph* graph;
+BaseNetflow *netflow;
 
 void Dialog::paintEvent(QPaintEvent *event)
 {
+    if (graph == nullptr)
+    {
+        return;
+    }
     Q_UNUSED(event);
     QPainter painter(this);
     painter.setPen(QPen(Qt::black, 1, Qt::SolidLine, Qt::FlatCap));
 
-    if (graph.nvertices == MAXV) return;
+    if (graph->nvertices == MAXV) return;
     painter.setRenderHint(QPainter::Antialiasing, true);
     painter.setPen(Qt::black);
     painter.setFont(QFont("times", 22));
-    for (int i = 0; i < graph.nvertices; ++i)
+    for (int i = 0; i < graph->nvertices; ++i)
     {
-        for (int j = i + 1; j < graph.nvertices; ++j)
+        for (int j = i + 1; j < graph->nvertices; ++j)
         {
             if (matrix[i][j] != 0)
             {
-                DrawLineWithArrow(painter, QPoint(graph.vertexCoord[i].x, graph.vertexCoord[i].y), QPoint(graph.vertexCoord[j].x, graph.vertexCoord[j].y));
+                DrawLineWithArrow(painter, QPoint(graph->vertexCoord[i].x, graph->vertexCoord[i].y), QPoint(graph->vertexCoord[j].x, graph->vertexCoord[j].y));
             }
             if (matrix[j][i] != 0)
             {
-                DrawLineWithArrow(painter, QPoint(graph.vertexCoord[j].x, graph.vertexCoord[j].y), QPoint(graph.vertexCoord[i].x, graph.vertexCoord[i].y));
+                DrawLineWithArrow(painter, QPoint(graph->vertexCoord[j].x, graph->vertexCoord[j].y), QPoint(graph->vertexCoord[i].x, graph->vertexCoord[i].y));
             }
         }
         painter.setBrush(QBrush(Qt::green, Qt::SolidPattern));
-        painter.drawEllipse(graph.vertexCoord[i].x - graph.r / 2, graph.vertexCoord[i].y - graph.r / 2, graph.r, graph.r);
+        painter.drawEllipse(graph->vertexCoord[i].x - graph->r / 2, graph->vertexCoord[i].y - graph->r / 2, graph->r, graph->r);
         painter.setFont(QFont("times", 22));
-        painter.drawText(graph.vertexCoord[i].x - graph.r / 5, graph.vertexCoord[i].y + graph.r / 5, QString::number(i+1));
+        painter.drawText(graph->vertexCoord[i].x - graph->r / 5, graph->vertexCoord[i].y + graph->r / 5, QString::number(i+1));
     }
 }
 
@@ -74,8 +79,8 @@ void Dialog::DrawLineWithArrow(QPainter& painter, QPoint start, QPoint end)
     double angle = std::atan2(-line.dy(), line.dx());
     QPoint vector = end - start;
     double length = sqrt(vector.x() * vector.x() + vector.y() * vector.y());
-    double xOffset = graph.r/2*((double)vector.x()) / length;
-    double yOffset = graph.r/2*((double)vector.y()) / length;
+    double xOffset = graph->r/2*((double)vector.x()) / length;
+    double yOffset = graph->r/2*((double)vector.y()) / length;
     QPoint offset(xOffset, yOffset);
     QPoint endpoint = end - offset;
     QPointF arrowP1 = endpoint + QPointF(sin(angle + M_PI / 3) * arrowSize, cos(angle + M_PI / 3) * arrowSize);
@@ -89,147 +94,24 @@ void Dialog::DrawLineWithArrow(QPainter& painter, QPoint start, QPoint end)
 
 void Dialog::on_pushButton_draw_vertices_clicked()
 {
-
-
-
-
-    //Graph g(8);
- //nf->insert_flow_edge(&g, 0, 1, 6);
- //nf->insert_flow_edge(&g, 0, 2, 6);
- //nf->insert_flow_edge(&g, 2, 1, 5);
- //nf->insert_flow_edge(&g, 2, 4, 9);
- //nf->insert_flow_edge(&g, 1, 3, 4);
- //nf->insert_flow_edge(&g, 1, 4, 2);
- //nf->insert_flow_edge(&g, 4, 3, 8);
- //nf->insert_flow_edge(&g, 4, 6, 7);
- //nf->insert_flow_edge(&g, 3, 6, 2);
- //nf->insert_flow_edge(&g, 3, 5, 4);
- //nf->insert_flow_edge(&g, 6, 5, 11);
- //nf->insert_flow_edge(&g, 6, 7, 4);
- //nf->insert_flow_edge(&g, 5, 7, 7);
-
-     FlowGraph g(14, true);
-     DinicNetflow *nf = new DinicNetflow();
-     nf->insert_flow_edge(&g, 0, 1, true, 6);
-     nf->insert_flow_edge(&g, 1, 0, true, 0);
-
-     nf->insert_flow_edge(&g, 0, 2, true, 6);
-     nf->insert_flow_edge(&g, 2, 0, true, 0);
-
-     nf->insert_flow_edge(&g, 0, 3, true, 8);
-     nf->insert_flow_edge(&g, 3, 0, true, 0);
-
-     nf->insert_flow_edge(&g, 0, 4, true, 9);
-     nf->insert_flow_edge(&g, 4, 0, true, 0);
-
-     nf->insert_flow_edge(&g, 1, 5, true, 4);
-     nf->insert_flow_edge(&g, 5, 1, true, 0);
-
-     nf->insert_flow_edge(&g, 1, 2, true, 3);
-     nf->insert_flow_edge(&g, 2, 1, true, 0);
-
-     nf->insert_flow_edge(&g, 2, 6, true, 4);
-     nf->insert_flow_edge(&g, 6, 2, true, 0);
-
-     nf->insert_flow_edge(&g, 2, 3, true, 4);
-     nf->insert_flow_edge(&g, 3, 2, true, 0);
-
-     nf->insert_flow_edge(&g, 3, 7, true, 5);
-     nf->insert_flow_edge(&g, 7, 3, true, 0);
-
-     nf->insert_flow_edge(&g, 3, 8, true, 10);
-     nf->insert_flow_edge(&g, 8, 3, true, 0);
-
-     nf->insert_flow_edge(&g, 3, 4, true, 3);
-     nf->insert_flow_edge(&g, 4, 3, true, 0);
-
-     nf->insert_flow_edge(&g, 4, 8, true, 6);
-     nf->insert_flow_edge(&g, 8, 4, true, 0);
-
-     nf->insert_flow_edge(&g, 5, 9, true, 5);
-     nf->insert_flow_edge(&g, 9, 5, true, 0);
-
-     nf->insert_flow_edge(&g, 5, 2, true, 9);
-     nf->insert_flow_edge(&g, 2, 5, true, 0);
-
-     nf->insert_flow_edge(&g, 6, 5, true, 8);
-     nf->insert_flow_edge(&g, 5, 6, true, 0);
-
-     nf->insert_flow_edge(&g, 6, 10, true, 5);
-     nf->insert_flow_edge(&g, 10, 6, true, 0);
-
-     nf->insert_flow_edge(&g, 6, 3, true, 10);
-     nf->insert_flow_edge(&g, 3, 6, true, 0);
-
-     nf->insert_flow_edge(&g, 7, 6, true, 8);
-     nf->insert_flow_edge(&g, 6, 7, true, 0);
-
-     nf->insert_flow_edge(&g, 7, 11, true, 5);
-     nf->insert_flow_edge(&g, 11, 7, true, 0);
-
-     nf->insert_flow_edge(&g, 7, 12, true, 12);
-     nf->insert_flow_edge(&g, 12, 7, true, 0);
-
-     nf->insert_flow_edge(&g, 8, 7, true, 7);
-     nf->insert_flow_edge(&g, 7, 8, true, 0);
-
-     nf->insert_flow_edge(&g, 8, 12, true, 7);
-     nf->insert_flow_edge(&g, 12, 8, true, 0);
-
-     nf->insert_flow_edge(&g, 9, 6, true, 10);
-     nf->insert_flow_edge(&g, 6, 9, true, 0);
-
-     nf->insert_flow_edge(&g, 9, 13, true, 6);
-     nf->insert_flow_edge(&g, 13, 9, true, 0);
-
-     nf->insert_flow_edge(&g, 10, 9, true, 8);
-     nf->insert_flow_edge(&g, 9, 10, true, 0);
-
-     nf->insert_flow_edge(&g, 10, 7, true, 12);
-     nf->insert_flow_edge(&g, 7, 10, true, 0);
-
-     nf->insert_flow_edge(&g, 10, 13, true, 9);
-     nf->insert_flow_edge(&g, 13, 10, true, 0);
-
-     nf->insert_flow_edge(&g, 11, 10, true, 8);
-     nf->insert_flow_edge(&g, 10, 11, true, 0);
-
-     nf->insert_flow_edge(&g, 11, 13, true, 7);
-     nf->insert_flow_edge(&g, 13, 11, true, 0);
-
-     nf->insert_flow_edge(&g, 12, 11, true, 7);
-     nf->insert_flow_edge(&g, 11, 12, true, 0);
-
-     nf->insert_flow_edge(&g, 12, 13, true, 6);
-     nf->insert_flow_edge(&g, 13, 12, true, 0);
-
-
-     std::cout << "Maximum flow " << nf->netflow(&g, 0, 13);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     int nvertices = ui->lineEdit_number_of_vertices->text().toInt();
-    if(nvertices != graph.nvertices)
+    if (ui->radioButton_EdmondsKarp->isChecked())
+    {
+        graph = new FlowGraph(MAXV, false);
+    }
+    else
+    {
+        graph = new FlowGraph(nvertices, true);
+    }
+    if(nvertices != graph->nvertices)
     {
         if (nvertices > MAXV)
         {
             QMessageBox::critical(this, "Ошибка", "Введенное число вершин превышает допустимое значение!");
             return;
         }
-        graph.nvertices = nvertices;
-        graph.GetCoordsArray();
+        graph->nvertices = nvertices;
+        graph->GetCoordsArray();
     }
     repaint();
 }
@@ -242,7 +124,7 @@ void Dialog::on_pushButton_add_clicked()
     int weight = str_weight.toInt();
     int vertex1 = -1;
     int vertex2 = -1;
-    for (int i = 0; i < graph.nvertices; ++i)
+    for (int i = 0; i < graph->nvertices; ++i)
     {
         if (str_vertex1 == QString::number(i+1))
         {
@@ -260,11 +142,22 @@ void Dialog::on_pushButton_add_clicked()
     else
     {
         matrix[vertex1][vertex2] = weight;
-        graph.nedges++;
-        netflow->insert_flow_edge(&graph, vertex1+1, vertex2+1, true, weight);
+        graph->nedges++;
+        if (netflow == nullptr)
+        {
+            if (ui->radioButton_EdmondsKarp->isChecked())
+            {
+                netflow = new EdmondsKarpNetflow();
+            }
+            else
+            {
+                netflow = new DinicNetflow();
+            }
+        }
+        netflow->insert_flow_edge(graph, vertex1+1, vertex2+1, true, weight);
 
         QString leText = ui->plainTextEdit_reslut->toPlainText();
-        leText.append("Edge: (%1, %2), weight:%3");
+        leText.append("Edge: (%1, %2), capacity:%3");
         leText = leText.arg(vertex1+1).arg(vertex2+1).arg(weight);
         leText.append("\n");
         ui->plainTextEdit_reslut->setPlainText(leText);
@@ -275,8 +168,8 @@ void Dialog::on_pushButton_add_clicked()
 void Dialog::on_pushButton_FindFlow_clicked()
 {
     int source, target;
-    graph.FindSourceAndTarget(source, target);
-    int total = netflow->netflow(&graph, source, target);
+    graph->FindSourceAndTarget(source, target);
+    int total = netflow->netflow(graph, source, target);
     print_result(total);
 }
 
@@ -284,9 +177,9 @@ void Dialog::print_result(int total)
 {
     EdgeNode *p;
     QString result;
-    for (int i=1; i<=graph.nvertices; i++)
+    for (int i=1; i<=graph->nvertices; i++)
     {
-        p = graph.edges[i];
+        p = graph->edges[i];
         while (p != NULL)
         {
             QString edgeInfo = "";
